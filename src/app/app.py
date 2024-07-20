bf1b5cbd6d4415947d485d283846ae2bed7f8a40
Pasin.K <jobpasin@hotmail.com>
2024-07-20 15:13:21 +0900

    Move plot graph into chatbot and create a class which contain interface

--- src/app/app.py
+++ src/app/app.py
@@ -6,127 +6,137 @@ from src.app.chatbot import MofuChatBot
 from src.constant.gui_text import *
 from functools import partial
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 def user(user_message, history):
     return "", history + [[user_message, None]]
 
 
-def bot(history, api_func):
-    response_message = api_func(history[-1][0])
-    history[-1][1] = ""
-    
-    if isinstance(response_message, dict):
-        graph_update = response_message
-        response_message = "Graph is updated"
+def temp_chat(input_text):
+    if input_text.lower() == "draw graph":
+        return {"Yo": 0}
     else:
-        graph_update = None
-    
-    for character in response_message:
-        history[-1][1] += character
-        time.sleep(0.01)
-        yield history, graph_update
-
-
-# Somehow I need one object from gradio here as argument.
-def reset_chat(button_obj, chatbot):
-    chatbot.reset()
-    # initial_question = mofu_bot.chat(None)
-    return [[None, MOFU_CHAN_INIT_PHRASE]]
-
-    
-
-def update_graph(data):
-    plt.figure()
-    plt.plot(data["income_assets"])
-    plt.title(data["commodities"])
-    plt.xlabel("X-axis")
-    plt.ylabel("Y-axis")
-    plt.grid(True)
-    return plt
+        return "User said: " + input_text
 
 
 
-def main():
-    theme = themes.Default()
+class MofuInterface:
+    def __init__(self, optimizer=None):
+        self.optimizer = optimizer        
+        self.chatbot = MofuChatBot()
+        self.call_chatbot = self.chatbot.chat
+        # self.call_chatbot = temp_chat
+        self.theme = themes.Default()
+        self.logo_path = "src/app/assets/mofu_logo.png"
+        # Custom CSS for chatbot background
+        self.custom_css = """
+        .chatbot-container {
+            background-color: #f0f8ff; /* Light blue color */
+        }
+        """
+        
+    def launch(self):
 
-    # Custom CSS for chatbot background
-    custom_css = """
-    .chatbot-container {
-        background-color: #f0f8ff; /* Light blue color */
-    }
-    """
-    logo_path = "src/app/assets/mofu_logo.png"
+        with gr.Blocks(theme=self.theme, css=self.custom_css) as demo:
+            gr.Markdown(f"<h1 style='text-align: center;'>{MOFU_CHAN_HEADER}</h1>")
+            gr.Markdown(f"<p style='text-align: left;'>{MOFU_CHAN_DESCRIPTION}</p>")
 
-    mofu_bot = MofuChatBot()
+            with gr.Row():
+                with gr.Column(scale=1):
+                    gr.Image(value=self.logo_path, width=300, show_label=False)
+                with gr.Column(scale=10):
+                    chatbot = gr.Chatbot(
+                        height=400,
+                        show_label=False,
+                        value=self.reset_chat(None),
+                        elem_classes="chatbot-container",
+                    )
 
-    # bot_func = partial(bot, api_func=mofu_bot.chat)
-
-    def bot_wrapper(history):
-        bot_generator = bot(history, mofu_bot.chat)
-        for h, graph_update in bot_generator:
-            if graph_update is not None:
-                yield h, update_graph(graph_update)
-            else:
-                yield h, None
-
-    with gr.Blocks(theme=theme, css=custom_css) as demo:
-        gr.Markdown(f"<h1 style='text-align: center;'>{MOFU_CHAN_HEADER}</h1>")
-        gr.Markdown(f"<p style='text-align: left;'>{MOFU_CHAN_DESCRIPTION}</p>")
-
-        with gr.Row():
-            with gr.Column(scale=1):
-                gr.Image(value=logo_path, width=300, show_label=False)
-            with gr.Column(scale=10):
-                chatbot = gr.Chatbot(
-                    height=400,
+            with gr.Row():
+                user_input = gr.Textbox(
+                    placeholder=MOFU_CHAN_TEXTBOX_PLACEHOLDER,
                     show_label=False,
-                    value=reset_chat(None, mofu_bot),
-                    elem_classes="chatbot-container",
+                    container=True,
+                    scale=7,
+                    interactive=True,
                 )
+                submit_btn = gr.Button("Send", variant="primary")
 
-        with gr.Row():
-            user_input = gr.Textbox(
-                placeholder=MOFU_CHAN_TEXTBOX_PLACEHOLDER,
-                show_label=False,
-                container=True,
-                scale=7,
-                interactive=True,
+
+            reset_btn = gr.Button("Reset")
+            mofu_input = gr.Textbox(visible=False, value=self.chatbot)
+            debug_btn = gr.Button("DEBUG")
+            
+
+            
+            submit_click = submit_btn.click(
+                user,
+                inputs=[user_input, chatbot],
+                outputs=[user_input, chatbot],
+                queue=False,
+            ).then(self.process_response, chatbot, chatbot)
+            submit_enter = user_input.submit(
+                user,
+                inputs=[user_input, chatbot],
+                outputs=[user_input, chatbot],
+                queue=False,
+            ).then(self.process_response, chatbot, chatbot)
+
+            reset_btn.click(
+                fn=self.reset_chat, inputs=mofu_input, outputs=chatbot
             )
-            submit_btn = gr.Button("Send", variant="primary")
+            debug_btn.click(
+                fn=self.chatbot.current_bot.get_history,
+                inputs=None,
+                outputs=None,
+            )
+            demo.launch()
 
-
-        reset_btn = gr.Button("Reset")
-        mofu_input = gr.Textbox(visible=False, value=mofu_bot)
-        debug_btn = gr.Button("DEBUG")
+    def process_response(self, history):
+        response_message = self.call_chatbot(history[-1][0])
         
-        graph_output = gr.Plot(label="Prediction")
+        if isinstance(response_message, dict):
+            x = np.linspace(0, 10, 100)
+            y = np.sin(x)
+            
+            fig, ax = plt.subplots()
+            ax.plot(x, y)
+            ax.set_title("Sine Wave")
+            ax.set_xlabel("X axis")
+            ax.set_ylabel("Y axis")
+            
+            # fig = go.Figure(data=go.Scatter(x=x, y=y, mode='lines'))
+            # fig.update_layout(
+            #     title="Sine Wave",
+            #     xaxis_title="X axis",
+            #     yaxis_title="Y axis"
+            # )
+            
+            history[-1][1] = gr.Plot(fig)
+            return history
+        else:
+            history[-1][1] = response_message
+            return history
+            # history[-1][1] = gr.Plot(fig)
+        #     yield history
+        # else:
+        #     history[-1][1] = ""
+        #     for character in response_message:
+        #         history[-1][1] += character
+        #         time.sleep(0.01)
+        #         yield history
 
-        
-        submit_click = submit_btn.click(
-            user,
-            inputs=[user_input, chatbot],
-            outputs=[user_input, chatbot],
-            queue=False,
-        ).then(bot_wrapper, chatbot, [chatbot, graph_output])
-        submit_enter = user_input.submit(
-            user,
-            inputs=[user_input, chatbot],
-            outputs=[user_input, chatbot],
-            queue=False,
-        ).then(bot_wrapper, chatbot, [chatbot, graph_output])
-
-        reset_btn.click(
-            fn=partial(reset_chat, chatbot=mofu_bot), inputs=mofu_input, outputs=chatbot
-        )
-
-        debug_btn = gr.Button("DEBUG")
-        debug_btn.click(
-            fn=mofu_bot.current_bot.get_history,
-            inputs=None,
-            outputs=None,
-        )
-        demo.launch()
+    # Somehow I need one object from gradio here as argument.
+    def reset_chat(self, button_obj):
+        self.chatbot.reset()
+        # initial_question = mofu_bot.chat(None)
+        return [[None, MOFU_CHAN_INIT_PHRASE]]
+    
+    
+def main():
+    app = MofuInterface()
+    app.launch()
 
 
 if __name__ == "__main__":
